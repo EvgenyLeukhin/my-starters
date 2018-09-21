606062346a1897753a70324078b1c20e38caef78
EvgenyL <eleukhin@codenetix.com>
2018-09-21 11:08:15 +0600

    gulp-materialize-starter

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 // LIBS
+//= ../node_modules/jquery/dist/jquery.min.js
 //= ../node_modules/materialize-css/dist/js/materialize.min.js
+
+$(document).ready(function () {
+  $('.materialboxed').materialbox();
+
+  $('.modal').modal();
+});
