52e16c08e32e3e1d3a4cdd945a304d7fbb82aff1
eleukhin <eleukhin@codenetix.com>
2018-10-05 00:21:40 +0600

    +++

--- src/components/Routes/index.jsx
+++ src/components/Routes/index.jsx
@@ -6,7 +6,6 @@ import Loading from './../Loading';
 // import fakeDelay from './../Loading/fakeDelay';
 
 // import pages w/o react-loadable (if tiny content)
-// import Home      from './../../pages/home';
 import Form      from './../../pages/form';
 import Form2     from './../../pages/form2';
 import Page404   from './../../pages/404';
@@ -14,15 +13,14 @@ import Page404   from './../../pages/404';
 // react-loadable (https://github.com/jamiebuilds/react-loadable-example) https://habr.com/post/325688/
 const loaderConstructor = path => {                         // (delayTime, path) - with fakeDelay
   return Loadable({
-    // loader: () => fakeDelay(delayTime).then(() => path), // with fakeDelay
+    // loader: () => fakeDelay(1000).then(() => path), // with fakeDelay
     loader: () => path,                                     // what need to load (dynamic import)
     loading: Loading,                                       // pre-loader component
   });
 };
 
 const pagesLoader = {
-  // home:      () => loaderConstructor(500, import('./../../pages/home')),  // with fakeDelay
-  home:      () => loaderConstructor(import('./../../pages/home')),
+  home:      () => loaderConstructor(import('./../../pages/home')),  // with fakeDelay
   redux:     () => loaderConstructor(import('./../../pages/redux')),
   bootstrap: () => loaderConstructor(import('./../../pages/bootstrap')),
   modal:     () => loaderConstructor(import('./../../pages/modal')),
