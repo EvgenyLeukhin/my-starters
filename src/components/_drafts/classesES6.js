632946dd077c9d6e02fe4b0013b7d2140e0fcdef
eleukhin <eleukhin@codenetix.com>
2018-10-07 10:18:40 +0600

    +++

---
+++ src/components/_drafts/classesES6.js
@@ -0,0 +1,45 @@
+// Весь React построен на классах (свойствах, методах и модульности)
+
+// Свойства класса без параметров
+class Someclass {
+  constructor() {
+    this.prop1 = 'Property1';
+    this.prop2 = 'Property2';
+  }
+}
+const classCopy = new Someclass();
+console.log(classCopy.prop1); // 'Property1';
+
+
+// Свойства класса с параметрами
+class Someclass {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+const classCopy = new Someclass('John', 32);
+const classCopy2 = new Someclass('Jack', 33);
+console.log(classCopy.name); // 'John';
+
+// Методы класса
+class Someclass {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  sayHelo() {
+    return `Hello, ${this.name}`;
+  }
+}
+
+const classCopy = new Someclass('John', 32);
+console.log(classCopy.greet());
+
+// Подкласс
+class Someclass2 extends Someclass {
+  constructor() {
+    super(name, age); // какие параметры  унаследовать от родительского класса
+    this.prop3 = 'New property'; // новое свойство
+  }
+}
---
+++ src/components/_drafts/modulesES6.js
@@ -0,0 +1,8 @@
+// file1.js
+export const some = 'Some text';
+export const num = 24;
+export default Someclass;
+
+// file2.js
+import { some, num } from './file1.js'
+import Someclass from './file1.js'
