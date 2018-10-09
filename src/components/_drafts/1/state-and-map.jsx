e187abb05a965c87c6b58d3f99bc12cc65958f52
Evgeny Leukhin <eugenel@effective.band>
2018-10-09 15:40:33 +0600

    +++

---
+++ src/components/_drafts/1/state-and-map.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    const { name, age } = this.props.contact;
+    return (
+      <div>
+        <p>{name}</p>
+        <p>{age}</p>
+      </div>
+    )
+  }
+}
+
+export default class Contacts extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [
+        {
+          id: 1,
+          name: 'John',
+          age: 32,
+        },
+        {
+          id: 2,
+          name: 'Jack',
+          age: 33,
+        },
+      ]
+    }
+  }
+  render() {
+    const {contacts} = this.state;
+    return (
+      <div>
+        {contacts.map(item => (
+          <Contact
+            name={item.name}
+            age={item.age}
+            key={item.id}
+          />
+
+          // <Contact contact={item} /> // short way
+        ))}
+      </div>
+    )
+  }
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,12 +5,56 @@ import { Link } from 'react-router-dom';
 
 import Mobile from './img/mobile.png';
 
+const data = {
+  persons: [
+    {
+      name: 'John',
+      age: 32,
+      city: 'Dallas',
+    },
+    {
+      name: 'Jack',
+      age: 33,
+      city: 'Toronto',
+    },
+  ]
+};
+
+
+const Person = props => {
+  const { name, age, city } = props.personProps;
+  return(
+    <div>
+      <span>{name}</span> - <span>{age}</span> - <b>{city}</b>
+    </div>
+  );
+};
+
 const Home = () => (
   <main className="home">
     <Helmet title='Home page' />
     <h1>Home page <FontAwesome name={'home'} spin={false} style={{ color: 'green' }} /></h1>
     <Link to='about'><img src={Mobile} alt="alt text" width={250} /> </Link>
     <p>Some content: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum numquam ipsum iure similique, culpa eum reiciendis tenetur ipsam, doloremque cupiditate molestiae! Totam quasi, dolorum reprehenderit vero ea eos culpa error?</p>
+    <hr/>
+    <h3>Maping data</h3>
+    <div>
+      {data.persons.map((item, index) =>
+        // Classical aproach
+        // <Person
+        //   name={item.name}
+        //   age={item.age}
+        //   city={item.city}
+        //   key={index}
+        // />
+
+        // short way
+        <Person
+          personProps={item}
+          key={index}
+        />
+      )}
+    </div>
   </main>
 );
 
