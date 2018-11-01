ac4b2218cdd63aa6d0f9d484877b6b25e5100b61
Evgeny Leukhin <eugenel@effective.band>
2018-11-01 13:41:05 +0600

    Add files

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,12 @@
+import React  from 'react';
+import './style.scss';
+
+export default class Layout extends React.Component {
+  render() {
+    return(
+      <div className="page-wrapper">
+        <h1>Some Content</h1>
+      </div>
+    );
+  }
+}
